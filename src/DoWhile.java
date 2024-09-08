f8bfe667c0aed1fd9b1153ea6f40331b167beea4
shirishb1 <sbandari@gitam.in>
2024-09-08 11:12:11 +0530

    shirish

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -1,6 +1,6 @@
 public class DoWhile {
-    public static void main(String[] args){
-        int count = 1;
+    public static void main(String[] args) {
+        //   int count = 1;
 //        while(count != 10){
 //            System.out.println("The count is: " + count);
 //            count++;
@@ -18,10 +18,34 @@ public class DoWhile {
 //        }
         //same logic with the do-while  loop
 
-        do{
-            System.out.println("The count is: " + count);
-            count++;
+//        do{
+//            System.out.println("The count is: " + count);
+//            count++;
+//
+//        }while(count != 6);
 
-        }while(count != 6);
+        // write a method to predict an odd number, if odd return true else return false.
+        //in the main using the while loop print all the odd numbers from 5 to 30.
+        int number = 5;
+        int maxvalue = 29;
+        while (number <= maxvalue){
+            number ++;
+            if (! OddNumberCheck(number)){
+                continue;
+            }
+            System.out.println("The odd number is: " + number);
+        }
+
+        boolean check = OddNumberCheck(3);
+        System.out.println(check);
+    }
+
+    public static boolean OddNumberCheck(int number){
+        if ((number % 2) == 1){
+            return true;
+
+        } else {
+            return false;
+        }
     }
 }
