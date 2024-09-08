e0b016bcb12ede81255fa2da62c48dfb5568e2c7
shirishb1 <sbandari@gitam.in>
2024-09-08 10:52:28 +0530

    shirish

---
+++ src/DoWhile.java
@@ -0,0 +1,27 @@
+public class DoWhile {
+    public static void main(String[] args){
+        int count = 1;
+//        while(count != 10){
+//            System.out.println("The count is: " + count);
+//            count++;
+//        }
+//        for(count = 1; count <= 10; count++){
+//            System.out.println("The count is: " + count);
+//        }
+
+//        while (true){
+//            if (count == 6){
+//                break;
+//            }
+//            System.out.println("The count is: " + count);
+//            count++;
+//        }
+        //same logic with the do-while  loop
+
+        do{
+            System.out.println("The count is: " + count);
+            count++;
+
+        }while(count != 6);
+    }
+}
