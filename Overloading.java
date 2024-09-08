617aadf1df714c2eff895b49eb17cc1ae1697c45
shirishb1 <sbandari@gitam.in>
2024-09-08 10:53:38 +0530

    Shirish

--- Overloading.java
+++ Overloading.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Overloading {
+    public static void main(String[] ags){
+        String Name = "Shirish";
+        int age = 22;
+        char Grade = "A";
+        double height = 179.5;
+        DisplayDetails(Name, age, Grade, Height);
+    }
+    public static void DisplayDetails(String UserName, int UserAge, char Grade, double height){
+        System.out.println("The name is : " + UserName + ". the age is : " + UserAge + ". The grade is : " + Grade + ". The height is : " + height);
+
+    }
 }
